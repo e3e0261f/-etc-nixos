255365d16fa873d59d5872c8a84b22c09308a306
kevin lee <e3e0261f@pm.me>
2026-09-01 10:25:28 +0800

    Save config (Flake): 2026-09-01 10:25:28

--- flake.nix
+++ flake.nix
@@ -1,15 +1,14 @@
 {
   description = "Rhys's NixOS Flake Configuration";
 
-  # 1. 輸入：定義你的資源來自哪裡
-  inputs = {
-    # 官方 NixOS 套件源 (使用你目前的 Unstable 分支)
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-    # 你的個人規則庫 (現在可以直接作為一個輸入！)
-    my-rules.url = "github:e3e0261f/GEoIP-GEoSITE";
-    my-rules.flake = false; # 因為你的庫不是一個 Flake，所以設為 false
-  };
+	# 1. 輸入：定義你的資源來自哪裡
+	inputs = {
+	  # 改成這個寫法，Nix 會自動下載極小的 tar.gz 壓縮包，而不是整個 3.5GB 的 Git 歷史
+	  nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+	  my-rules.url = "github:e3e0261f/GEoIP-GEoSITE";
+	  my-rules.flake = false;
+	};
 
   # 2. 輸出：定義如何構建你的系統
   outputs = { self, nixpkgs, my-rules, ... }@inputs: {
