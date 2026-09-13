59d22b11ddc2261e0ddc479feca2db247cc9f1a6
kevin lee <e3e0261f@pm.me>
2026-09-13 19:50:24 +0800

    Save config: 2026-09-13 19:50:24

--- modules/home.nix
+++ modules/home.nix
@@ -25,51 +25,9 @@
     ./rhys.nix
   ];
 
-  #   # 1. 强制声明 GTK 与 Qt 图标主题统一为 Papirus-Dark
-  # gtk = {
-  #   enable = true;
-  #   iconTheme = {
-  #     name = "Papirus-Dark";
-  #     package = pkgs.papirus-icon-theme;
-  #   };
-  # };
-
-  # # 2. 补齐 Fcitx5 自带的图标数据包，并加入 XDG 环境变量
-  # home.packages = with pkgs; [
-  #   papirus-icon-theme
-  #   adwaita-icon-theme
-  #   hicolor-icon-theme
-  #   fcitx5-material-color # 为 Fcitx5 提供完整的 Material 图标与配色支持
-  # ];
-
-  # # 3. 告诉 Qt 应用去哪里搜寻系统图标
-  # home.sessionVariables = {
-  #   XDG_DATA_DIRS = "$XDG_DATA_DIRS:${pkgs.papirus-icon-theme}/share:${pkgs.adwaita-icon-theme}/share";
-  # };
-
   home.sessionVariables = {
     # 這是所有 GTK 程式 (包含你彈出的通知、輸入法設定) 的字體大小總開關
     GTK_FONT_NAME = "Noto Sans CJK TC 16";
-  };
-
-  # 在 home-manager (home.nix) 中：
-  systemd.user.services.quickshell = {
-    Unit = {
-      Description = "QuickShell Desktop Shell";
-      PartOf = [ "graphical-session.target" ];
-      After = [ "graphical-session.target" ];
-    };
-    Service = {
-      # 如果通过 flake 安装，填写 inputs.quickshell 或 pkgs.quickshell 的路径；也可以直接写 "qs"
-      ExecStart = "${pkgs.quickshell}/bin/qs";
-      Restart = "on-failure";       # 崩溃时自动重启
-      RestartSec = "1s";
-          # ⭐️ 核心关键：强行给 Systemd 注入此环境变量
-    Environment = [ "QS_ICON_THEME=Papirus-Dark" ];
-    };
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
-    };
   };
 
   # 💡 確保 Waybar 由 Systemd 管理，並掛載在 Hyprland 會話上
@@ -89,7 +47,9 @@
     };
 
     Service = {
-      # 填入可执行文件名称
+      # ⭐️ 把这一行加进去（之前就是漏了它）：
+      Environment = [ "QS_ICON_THEME=Papirus-Dark" ];
+
       ExecStart = "${inputs.caelestia-shell.packages.${pkgs.system}.default}/bin/caelestia-shell";
       Restart = "on-failure";
       RestartSec = "1s";
