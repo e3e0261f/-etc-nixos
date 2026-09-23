454ba239019a4ead2f0944cc5e0c54359d59fe93
kevin lee <e3e0261f@pm.me>
2026-09-24 02:23:26 +0800

    Save config: 2026-09-24 02:23:26

--- configuration.nix
+++ configuration.nix
@@ -76,6 +76,14 @@
   };
 
   nix.settings = {
+    substituters = [
+      "https://ustc.edu.cn" # 1. 优先尝试科大
+      "https://tsinghua.edu.cn" # 2. 科大没有或挂了，走清华
+      "https://nixos.org" # 3. 都没找到，走官方源
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
     # max-jobs = 16;
     max-jobs = "auto";
     cores = 0; 
